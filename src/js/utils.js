d5ac4726654e1c04caf718fe6861ea562f162339
AdrianGerman <adrian.german1019@gmail.com>
2024-01-24 12:07:19 -0600

    feat: statuses for tasks

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
-import { addTodo } from "./store";
+import { addTodo, updateTodo } from "./store";
 import { todoTemplateCreator } from "./templates";
 
 export const addTodoController = (text) => {
@@ -11,6 +11,11 @@ export const addTodoController = (text) => {
   const $todoElement = document.createElement(null);
   const todoTemplate = todoTemplateCreator(newTodo);
   $todoElement.innerHTML = todoTemplate;
+  //Agregando eventos
+  const $todoStatusCheckbox = $todoElement.querySelector("input.toggle");
+  $todoStatusCheckbox.addEventListener("change", (e) =>
+    updateTodoStatus(newTodo.id, e.target.checked)
+  );
   // Agrega nodo al HTML
   const $todoList = document.querySelector(".todo-list");
   $todoList.appendChild($todoElement.firstElementChild);
@@ -18,4 +23,13 @@ export const addTodoController = (text) => {
   const $todoContainer = document.querySelector(".todoapp-wrapper");
   if ($todoContainer.classList.contains("inactive"))
     $todoContainer.classList.remove("inactive");
+};
+
+export const updateTodoStatus = (todoId, isCompleted) => {
+  // Actualizar TODO en el estado global
+  updateTodo(todoId, { completed: isCompleted });
+  // Modificando estilos de HTML
+  const $todoContainer = document.querySelector(`li[data-todo-id="${todoId}"]`);
+  if (isCompleted) $todoContainer.classList.add("completed");
+  else $todoContainer.classList.remove("completed");
 };
